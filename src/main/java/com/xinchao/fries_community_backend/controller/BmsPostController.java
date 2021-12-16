1a3e97d63845feacec06970daf5627e8ff8d0143
Potato <w17666104677@outlook.com>
2021-12-16 15:55:40 +0800

    更新/删除帖子

--- src/main/java/com/xinchao/fries_community_backend/controller/BmsPostController.java
+++ src/main/java/com/xinchao/fries_community_backend/controller/BmsPostController.java
@@ -1,6 +1,7 @@
 package com.xinchao.fries_community_backend.controller;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.vdurmont.emoji.EmojiParser;
 import com.xinchao.fries_community_backend.common.api.ApiResult;
 import com.xinchao.fries_community_backend.model.dto.CreateTopicDTO;
 import com.xinchao.fries_community_backend.model.entity.BmsPost;
@@ -8,9 +9,12 @@ import com.xinchao.fries_community_backend.model.entity.UmsUser;
 import com.xinchao.fries_community_backend.model.vo.PostVO;
 import com.xinchao.fries_community_backend.service.IBmsPostService;
 import com.xinchao.fries_community_backend.service.IUmsUserService;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
+import javax.validation.Valid;
 
+import java.util.Date;
 import java.util.Map;
 
 import static com.xinchao.fries_community_backend.jwt.JwtUtil.USER_NAME;
@@ -53,5 +57,26 @@ public class BmsPostController extends BaseController{
     public ApiResult<Map<String, Object>> view(@RequestParam("id") String id) {
         Map<String, Object> map = iBmsPostService.viewTopic(id);
         return ApiResult.success(map);
+    }
+
+    //帖子更新与删除
+    @PostMapping("/update")
+    public ApiResult<BmsPost> update(@RequestHeader(value = USER_NAME) String userName, @Valid @RequestBody BmsPost post) {
+        UmsUser umsUser = umsUserService.getUserByUsername(userName);
+        Assert.isTrue(umsUser.getId().equals(post.getUserId()), "非本人无权修改");
+        post.setModifyTime(new Date());
+        post.setContent(EmojiParser.parseToAliases(post.getContent()));
+        iBmsPostService.updateById(post);
+        return ApiResult.success(post);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ApiResult<String> delete(@RequestHeader(value = USER_NAME) String userName, @PathVariable("id") String id) {
+        UmsUser umsUser = umsUserService.getUserByUsername(userName);
+        BmsPost byId = iBmsPostService.getById(id);
+        Assert.notNull(byId, "话题已不存在");
+        Assert.isTrue(byId.getUserId().equals(umsUser.getId()), "你无权删除别人的话题");
+        iBmsPostService.removeById(id);
+        return ApiResult.success(null,"删除成功");
     }
 }
